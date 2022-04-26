c3012cbf032a8c3dd0356e91ceaf3324278cf0a6
Andrew <brokenhoax@outlook.com>
2022-04-26 08:45:06 -0500

    updating dependencies and server port so as to not conflict with front-end

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const routes = require("./routes/routes");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
